15cfa2a23e5bef51bd96af9931d9b64e2c9ecb80
Francis Ventura <francis.ventura@dxbdmac3.local>
2019-10-21 17:14:50 +0400

    Restructured folders, exported svg icons

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
-    cssnano = require("cssnano"),
-    sourcemaps = require("gulp-sourcemaps"),
-    browsersync = require("browser-sync").create();
-
