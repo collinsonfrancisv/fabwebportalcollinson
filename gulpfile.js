156c33fe4925b0a2ee25d47eef66db0f963c2114
Francis Ventura <francisfventura@gmail.com>
2019-10-21 08:03:27 +0400

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    sourcemaps = require("gulp-sourcemaps"),
+    browsersync = require("browser-sync").create();
+
